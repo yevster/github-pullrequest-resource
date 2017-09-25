c2b64b56117da57f1860f9295609d6edbb95de91
Yev Bronshteyn <ybronshteyn@blackducksoftware.com>
2017-09-25 13:41:36 -0400

    Inital commit - customizable detail URL

--- assets/lib/status.rb
+++ assets/lib/status.rb
@@ -1,8 +1,8 @@
 require 'octokit'
 
 class Status
-  def initialize(state:, atc_url:, sha:, repo:, title:, description:, context: 'concourse-ci')
-    @atc_url = atc_url
+  def initialize(state:, detail_url:, sha:, repo:, title:, description:, context: 'concourse-ci')
+    @detail_url = detail_url
     @context = context
     @repo    = repo
     @sha     = sha
@@ -18,13 +18,11 @@ class Status
       @state,
       context: "#{@title}/#{@context}",
       description: @description,
-      target_url: target_url
+      target_url: detail_url
     )
   end
 
   private
 
-  def target_url
-    "#{@atc_url}/builds/#{ENV['BUILD_ID']}" if @atc_url
-  end
+
 end
